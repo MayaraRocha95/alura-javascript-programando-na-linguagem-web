fe94a77c941636c2bb6b931911f8888bbbd4dad2
Mayara Rocha <mayaraaraujorocha54@gmail.com>
2022-09-15 15:55:10 -0300

    Calculando o imc

--- js/principal.js
+++ js/principal.js
@@ -1,5 +1,30 @@
-console.log("estou funcionado")
-
 var titulo = document.querySelector(".titulo");
+titulo.textContent = "Aparecida Nutricionista";
 
-titulo.textContent = "Aparecida Nutricionista"; // alterando o conteúdo de texto
+var paciente = document.querySelector("#paciente");
+
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent; //aqui vou dizer que a altura é o meru texto
+
+var tdImc = paciente.querySelector(".info-imc");
+
+var pesoValido = true;
+var alturaValida = true;
+
+if(peso <= 0 || peso >= 1000){
+  pesoValido = false;
+  tdImc.textContent = "Peso Inválido!";
+}
+if (altura <= 0 || altura >= 3.00){
+   alturaValida = false;
+  tdImc.textContent = "Altura Inválido!";
+}
+if (alturaValida && pesoValido){
+  var imc = peso / (altura * altura);
+  tdImc.textContent = imc;
+
+}
+
